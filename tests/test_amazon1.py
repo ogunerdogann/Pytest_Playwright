78558d7c104c7db696b541aa8e17a5b5d48660ad
Erdoğan <Oguen.Erdogan@rommelag.com>
2024-06-06 16:20:41 +0200

    06062024V4

--- tests/test_amazon1.py
+++ tests/test_amazon1.py
@@ -1,19 +1,11 @@
 import re 
-from playwright.sync_api import Page,expect,Locator
+import pytest
+from playwright.sync_api import Page,expect
+from configuration_properties import go_to_amazon,negative_login
 
-amazon_url = "https://www.amazon.de/"
-def test_has_title(page:Page):
-    page.goto(amazon_url)
-
-    # Expect a title "to contain" a substring
-    expect(page).to_have_title(re.compile("Günstige"))
 
 def test_get_started_link(page:Page):
-    page.goto(amazon_url)
-
-    # Click the Akzeptieren(cookies) button
-    page.locator("css=input[name='accept']").click()
-    
+   
     # Expects page to have a heading with the name of installation
-    expect(page.locator("css=#nav-logo-sprites")).to_be_visible()
+    expect(page.locator("css=[type='submit']")).to_be_visible()
 
