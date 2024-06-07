81e0e7ae5348d63a3ebfbf9d41f7d130e95bc0fa
Erdoğan <Oguen.Erdogan@rommelag.com>
2024-06-07 16:38:58 +0200

    07062024

--- tests/test_amazon1.py
+++ tests/test_amazon1.py
@@ -14,4 +14,4 @@ def test_get_started_link(page:Page, go_to_amazon):
 
     for i in range(0,len(Amazon_MainPage.NAVBAR)):
         expect(page.get_by_role("link", name=Amazon_MainPage.NAVBAR[i]).first).to_be_visible()
-        
+        #github
