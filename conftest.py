351cc4c504a11af2ef4af7c0cc9538171e6978d8
Ogün Erdogan <ogun.erdogan23@gmail.com>
2024-06-12 22:28:28 +0200

    12062024

--- conftest.py
+++ conftest.py
@@ -1,3 +1,24 @@
-pytest_plugins = [
-    "tests.configuration_properties"
-]
+import pytest
+import re
+from playwright.sync_api import Page,expect
+
+
+
+pytest_plugins = [  ]
+
+def pytest_addoption(parser) -> None:
+    parser.addoption(
+        "--amazon_base_url",
+        action = "store",
+        default = "https://www.amazon.de/",
+        help = "Amazon main page URL"
+    )
+
+
+@pytest.fixture(scope = "function")
+def go_to_amazon_main_page(request, page:Page):
+    page.goto(request.config.getoption("--amazon_base_url"))
+    # Click the Akzeptieren(cookies) button
+    button = page.locator("css=input[name='accept']")
+    button.click()
+    expect(page).to_have_title(re.compile("Günstige"))
--- tests/test_amazon1.py
+++ tests/test_amazon1.py
@@ -3,15 +3,15 @@ import pytest
 from playwright.sync_api import Page,expect
 from amazon_mainPage import Amazon_MainPage
 
-
-def test_get_started_link(page:Page, go_to_amazon):
-   
-    deneme = "css=[type='submit']"
+@pytest.mark.test1
+def test_get_started_link(page:Page, go_to_amazon_main_page):
+     
     # Expects page to have a heading with the name of installation
     expect(page.locator(Amazon_MainPage.AMAZON_LOGO)).to_be_visible()
     expect(page.locator(Amazon_MainPage.SEARCH_BOX)).to_be_visible()
     expect(page.get_by_role("link", name="Amazon Basics ")).to_be_visible()
 
     for i in range(0,len(Amazon_MainPage.NAVBAR)):
+        page.wait_for_timeout(3000)
         expect(page.get_by_role("link", name=Amazon_MainPage.NAVBAR[i]).first).to_be_visible()
-        #github
+        
