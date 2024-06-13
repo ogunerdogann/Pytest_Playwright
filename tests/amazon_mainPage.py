5ef8b66cbc1fa9356f6ce82eb616f864a379d484
Erdoğan <Oguen.Erdogan@rommelag.com>
2024-06-13 15:16:12 +0200

    13062024V2

--- tests/amazon_mainPage.py
+++ tests/amazon_mainPage.py
@@ -1,9 +1,12 @@
+from enum import Enum,auto
 
 
 class Amazon_MainPage:
 
     AMAZON_LOGO = "a[aria-label='Amazon.de']"
     SEARCH_BOX = "input[id='twotabsearchtextbox']"
+    SEARCH_SUBMIT_BUTTON = "input[id='nav-search-submit-button']"
+    SEARCH_WORD = "div[class='sg-col-inner']>div>span[class='a-color-state a-text-bold']"
     NAVBAR = ["Amazon Basics ",
               "Neuerscheinungen ",
               "Musik",
@@ -15,4 +18,14 @@ class Amazon_MainPage:
               "Lebensmittel",
               "Küche, Haushalt & Wohnen"]
     PRIME_STUDENT_TEXT = "a[aria-label='Student']"
-    DENEME = 0 #github baglanmaya calisiliyor!!!
+    FOOTER_ELEMENT = "td.navFooterDescItem"      #"span[class='navFooterDescText']"  # !! index 9
+
+class Search_Words(Enum):
+    NUTELLA = 1
+    DORITOS = auto()
+    BOUNTY = auto()
+    MILKA = auto()
+    PEPSI = auto()
+    MANGO = auto()
+    MORITZ_FIEGE = auto() 
+
