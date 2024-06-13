743749f2749fb4e7aa6d68b7fdb26d2aa1edeaf6
Erdoğan <Oguen.Erdogan@rommelag.com>
2024-06-13 15:26:39 +0200

    13062024V6

--- tests/test_amazon1.py
+++ tests/test_amazon1.py
@@ -4,10 +4,10 @@ import pytest
 
 class Amazon_MainPageTests:
 
-    button = "hello"
-
+   button = 'hello'
+   
 @pytest.mark.test1
-def test_get_started_link(page:Page, main_page):
+def test_get_started_link(page:Page, go_to_amazon_main_page):
    
     #deneme = "css=[type='submit']"
     # Expects page to have a heading with the name of installation
@@ -19,7 +19,7 @@ def test_get_started_link(page:Page, main_page):
         expect(page.get_by_role("link", name=Amazon_MainPage.NAVBAR[i]).first).to_be_visible()
 
 @pytest.mark.test2       
-def test_nutella(page:Page, main_page):
+def test_nutella(page:Page, go_to_amazon_main_page):
     search_word = ""
     for i in range(1,len(list(Search_Words))+1):
         search_word = Search_Words(i).name
@@ -29,11 +29,11 @@ def test_nutella(page:Page, main_page):
         expect(page.locator(Amazon_MainPage.SEARCH_WORD)).to_contain_text(search_word)
     
 @pytest.mark.test3
-def test_footer(page:Page, main_page):
+def test_footer(page:Page, go_to_amazon_main_page):
     
     page.mouse.wheel(0,13188)
     page.wait_for_timeout(5000)
     for i in range(0,12):
-        expect(page.locator(Amazon_MainPage.FOOTER_REDUZIERTE_BWARE).nth(i)).to_be_visible()
+        expect(page.locator(Amazon_MainPage.FOOTER_ELEMENT).nth(i)).to_be_visible()
 
         
