a307c6ceaf6bb7caa75fcf16518d729d78c3f1c9
Erdoğan <Oguen.Erdogan@rommelag.com>
2024-06-14 16:04:54 +0200

    14062024V3

--- tests/test_amazon1.py
+++ tests/test_amazon1.py
@@ -16,7 +16,7 @@ def test_get_started_link(page:Page):
     page.wait_for_timeout(1000)
     expect(page.locator(Amazon_MainPage.AMAZON_LOGO)).to_be_visible()
     expect(page.locator(Amazon_MainPage.SEARCH_BOX)).to_be_visible()
-    expect(page.get_by_role("link", name="Amazon Basics ")).to_be_visible()
+    expect(page.get_by_role("link", name="Amazon Basics1 ")).to_be_visible()
 
     for i in range(0,len(Amazon_MainPage.NAVBAR)):
         expect(page.get_by_role("link", name=Amazon_MainPage.NAVBAR[i]).first).to_be_visible()
