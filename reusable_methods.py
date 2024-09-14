ebcefb54f12b9ca46bd15d0aa3da250ceee30046
Ogün Erdogan <ogun.erdogan23@gmail.com>
2024-09-14 18:45:27 +0200

    rebase_deneme_commit

--- reusable_methods.py
+++ reusable_methods.py
@@ -1,5 +1,9 @@
 from playwright.sync_api import Page,Locator
 
+
+def go_to_main_page():
+    """"""
+
 class _RootComponent:
     """
     Root Component Class
@@ -25,15 +29,21 @@ class UnitActionButton(_RootComponent):
     """
     This class decribes the behavior of a button
     """
-    def __init__(self, page: Page, parent: Locator, testid: str) -> None:
-        locator = parent.get_by_test_id(testid)
-        super().__init__(page, locator)
+    
+    def __init__(self, page: Page, parent: Locator, test_id: str) -> None:
+        locator = parent.get_by_test_id(test_id)
+        super().__init__(page=page, locator=locator)
 
     def click(self) -> None:
         """
         Clicks on the relevant button
         """
         self.root.click()
+
+    def get_title(self) -> str:
+        """Returns the title of a button"""
+        title = self.root.text_content()
+        return title if title is not None else ""
 
     def wait_until_visible(self,timeout: 5000) -> None:
         """
